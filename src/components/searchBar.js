8df0f08372ef7b85b4ace5db1e8670da1b29cac1
Rory Letteney <rory.letteney@gmail.com>
2018-10-03 12:33:25 -0500

    Styled the searchbar component

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 
 class SearchBar extends Component {
-
-    handleFormSubmit = function({query}) {
-        this.props.onSubmit(query);
-    }.bind(this);
+  handleFormSubmit = function({ query }) {
+    this.props.onSubmit(query);
+  }.bind(this);
 
   renderInput(field) {
     return (
@@ -18,8 +17,14 @@ class SearchBar extends Component {
   render() {
     const { handleSubmit } = this.props;
     return (
-      <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit)}>
-        <Field name="query" component={this.renderInput} />
+      <form
+        className="search-bar"
+        onSubmit={handleSubmit(this.handleFormSubmit)}
+      >
+        <div className="search-bar__wrapper">
+          <Field name="query" component={this.renderInput} />
+          <p>Press return to search</p>
+        </div>
       </form>
     );
   }
