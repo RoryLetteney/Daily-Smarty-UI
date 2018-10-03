0c40d641e4883e5965dd31b64d1a77fd9096e658
Rory Letteney <rory.letteney@gmail.com>
2018-10-03 12:12:23 -0500

    Started styling the home component

--- src/components/home.js
+++ src/components/home.js
@@ -1,30 +1,30 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-import * as actions from '../actions';
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 import Logo from "./logo";
 import SearchBar from "./searchBar";
 import RecentPosts from "./recentPosts";
 
 class Home extends Component {
-
   handleSearchBarSubmit = function(query) {
     this.props.fetchPostsWithQuery(query, () => {
       this.props.history.push("/results");
-    });    
-  }
+    });
+  };
 
   render() {
     return (
-      <div>
-        <div className="home">
-          <Logo />
-          <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
-          <RecentPosts />
-        </div>
+      <div className="home">
+        <Logo />
+        <SearchBar onSubmit={query => this.handleSearchBarSubmit(query)} />
+        <RecentPosts />
       </div>
     );
   }
 }
 
-export default connect(null, actions)(Home);
+export default connect(
+  null,
+  actions
+)(Home);
