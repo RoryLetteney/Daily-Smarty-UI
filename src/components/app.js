a626cb6176e4ac33d50bc1c9f09198b9827e36ae
Rory Letteney <rory.letteney@gmail.com>
2018-10-03 07:16:07 -0500

    Setup logo component

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import Logo from './logo';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <div>
-          logo
+        
+        <div className="logo">
+          <Logo/>
         </div>
+        
+        <div className="search">
+          <input placeholder="Daily Smarty"/>
+        </div>
+        
+        <div className="recent">
+          <ul>
+            <li>post</li>
+            <li>post</li>
+            <li>post</li>
+          </ul>
+        </div>
+      
       </div>
     );
   }
