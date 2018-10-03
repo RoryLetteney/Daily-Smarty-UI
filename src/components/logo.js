f26cedf579dce43db5ab6d35dff45e0b1eb1878a
Rory Letteney <rory.letteney@gmail.com>
2018-10-03 11:21:37 -0500

    Mapped state to props in resultsPosts component and rendered each post with post component. Also added link to logo to go to home

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,17 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import { Link } from "react-router-dom";
 
 class Logo extends Component {
-   render() {
-       const size ={
-           height: this.props.size ? this.props.size : 100,
-           width: this.props.size ? this.props.size : 100
-       }
-       return (
-           <div className="logo-main">
-               <img style={size} alt="Daily Smarty Logo Image" src="/assets/ds_circle_logo.png"/>
-           </div>
-       )
-   }
+  render() {
+    const size = {
+      height: this.props.size ? this.props.size : 100,
+      width: this.props.size ? this.props.size : 100
+    };
+    return (
+      <div className="logo-main">
+        <Link to="/">
+          <img
+            style={size}
+            alt="Daily Smarty Logo Image"
+            src="/assets/ds_circle_logo.png"
+          />
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default Logo;
